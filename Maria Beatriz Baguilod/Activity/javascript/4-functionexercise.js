605720068b1be60019fbcd48e4b388da3125a29c
Chette Baguilod <143865718+dev-mbcbaguilod@users.noreply.github.com>
2023-10-14 09:11:57 +0800

    Delete Maria Beatriz Baguilod/Activity/javascript directory

--- Maria Beatriz Baguilod/Activity/javascript/4-functionexercise.js
+++
@@ -1,7 +0,0 @@
-function capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1); // charAt to get the letter at a specific position (0 - first letter); toUpperCase is to capitalize; str.slice(1) to include letters following the first letter
-}
-
-console.log(capitalize('eggplant'));
-console.log(capitalize('pamplemousse'));
-console.log(capitalize('squid'));
